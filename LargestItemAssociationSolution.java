317cd596520c25286581e440347a7315f77a997c
Nitish Rane <rane.nitish@gmail.com>
2020-09-05 18:34:15 -0700

    Add problem description and complexity
    Add problem description and time & space complexity

--- LargestItemAssociationSolution.java
+++ LargestItemAssociationSolution.java
@@ -1,3 +1,5 @@
+/*Largest item association: Given pairs of item association, one needs to find the largest item association. If we have ties, then the association that has lower items in terms of lexicographical order will be selected. For instance, if item5 -> item6 -> item7 & item5 -> item6 -> item8 are a tie, then the former one will be chosen*/
+
 import java.util.*;
 
 class PairString{
@@ -10,77 +12,80 @@ class PairString{
 	}
 }
 
-class LargestItemAssociationSolution{
-    //You may add your test cases here in the form of PairString objects
-    //and add them to the List<PairString>
-    public static void main(String args[]){
-	List<PairString> itemAssociation = new ArrayList();
-	itemAssociation.add(new PairString("item1", "item2"));
-	itemAssociation.add(new PairString("item2", "item3"));
-	itemAssociation.add(new PairString("item2", "item4"));
-	itemAssociation.add(new PairString("item5", "item6"));
-	itemAssociation.add(new PairString("item6", "item7"));
-	List<String> result = largestItemAssociation(itemAssociation);
-	result.forEach(System.out::println);
-     }
-	
-     public static List<String> largestItemAssociation(List<PairString> itemAssociation){
-	  Map<String, Set<String>> itemAssociationMap = new HashMap();
-		
-	  //Form an adjacency list
-	  for(PairString ps: itemAssociation){
-	  	Set<String> tempAssociationSet = itemAssociationMap.getOrDefault(ps.first, new HashSet());
-		tempAssociationSet.add(ps.second);
-		itemAssociationMap.put(ps.first,tempAssociationSet);
-		tempAssociationSet = itemAssociationMap.getOrDefault(ps.second, new HashSet());
-		tempAssociationSet.add(ps.first);
-		itemAssociationMap.put(ps.second,tempAssociationSet);		
-	  }
-	  List<String> result = new LinkedList();
+class largestItemAssociationSolution{
 
-	  //For every item as a start, do a Depth First search for associations
-	  for(String item: itemAssociationMap.keySet()){
-	  	List<String> tempresult = DFS_iterative(item, itemAssociationMap, new HashSet(), new LinkedList(), new LinkedList());
-      	  	if(doesAreplaceB(tempresult, result)){
-        		result = getClonedList(tempresult);
-      		}
-    	  }
-	  return result;
+	//You may add your test cases here in the form of PairString objects
+	//and add them to the List<PairString>
+	public static void main(String args[]){
+		List<PairString> itemAssociation = new ArrayList();
+		itemAssociation.add(new PairString("item1", "item2"));
+		itemAssociation.add(new PairString("item2", "item3"));
+		itemAssociation.add(new PairString("item2", "item4"));
+		itemAssociation.add(new PairString("item5", "item6"));
+		itemAssociation.add(new PairString("item6", "item7"));
+		List<String> result = largestItemAssociation(itemAssociation);
+		result.forEach(System.out::println);
+	}
+	
+	public static List<String> largestItemAssociation(List<PairString> itemAssociation){
+		Map<String, Set<String>> itemAssociationMap = new HashMap();
+		
+		//Form an adjacency list
+		for(PairString ps: itemAssociation){
+			Set<String> tempAssociationSet = itemAssociationMap.getOrDefault(ps.first, new HashSet());
+			tempAssociationSet.add(ps.second);
+			itemAssociationMap.put(ps.first,tempAssociationSet);
+			tempAssociationSet = itemAssociationMap.getOrDefault(ps.second, new HashSet());
+			tempAssociationSet.add(ps.first);
+			itemAssociationMap.put(ps.second,tempAssociationSet);		
+		}
+
+		List<String> result = new LinkedList();
+	
+		//For every item as a start, do a Depth First search for associations
+		for(String item: itemAssociationMap.keySet()){
+			List<String> tempresult = DFS_iterative(item, itemAssociationMap, new HashSet(), new LinkedList(), new LinkedList());
+
+      if(doesAreplaceB(tempresult, result)){
+        result = getClonedList(tempresult);
       }
+    }
+		return result;
+	}
 	
 	public static List<String> DFS_iterative(String item, Map<String, Set<String>> itemAssociationMap, Set<String> visitedItem, List<String> result, List<String> tempresult){
-	     Stack<String> stack = new Stack();
-	     stack.push(item);
+		Stack<String> stack = new Stack();
+		stack.push(item);
 		
-	     while(!stack.isEmpty()){
-	     String currentItem = stack.peek();
-	     if(visitedItem.contains(currentItem)){
-		stack.pop();
-		if(tempresult.size()>0) tempresult.remove(tempresult.size()-1);
-		continue;
-	     }
+		while(!stack.isEmpty()){
+			String currentItem = stack.peek();
+			if(visitedItem.contains(currentItem)){
+				stack.pop();
+				if(tempresult.size()>0) tempresult.remove(tempresult.size()-1);
+				continue;
+			}
 			
-	     visitedItem.add(currentItem);
-	     tempresult.add(currentItem);
-	     boolean hasAllAssociationsVisited = true;
+			visitedItem.add(currentItem);
+			tempresult.add(currentItem);
+			boolean hasAllAssociationsVisited = true;
 			
-	     for(String eachItem: itemAssociationMap.get(currentItem)){
-		if(!visitedItem.contains(eachItem)){
-			stack.push(eachItem);
-			hasAllAssociationsVisited = false;
+			for(String eachItem: itemAssociationMap.get(currentItem)){
+				if(!visitedItem.contains(eachItem)){
+					stack.push(eachItem);
+					hasAllAssociationsVisited = false;
+				}
+			}
+			
+			//Means we have reached the end of an association
+			//We need to store the association or compare it with the result association
+			if(hasAllAssociationsVisited){
+				if(doesAreplaceB(tempresult, result)){
+				  result = getClonedList(tempresult);
+				}
+			}
 		}
-	      }
-			
-	      //Means we have reached the end of an association
-	      //We need to store the association or compare it with the result association
-	      if(hasAllAssociationsVisited){
-	      	   if(doesAreplaceB(tempresult, result)){
-			result = getClonedList(tempresult);
-		   }
-	       }
-         }
-         return result;
-  }
+    return result;
+	}
 
   private static List<String> getClonedList(List<String> sourceList){
       List<String> targetList = new LinkedList();
@@ -90,16 +95,20 @@ class LargestItemAssociationSolution{
       return targetList;
   }
 	
-
-   private static boolean doesAreplaceB(List<String> A, List<String> B){
-      if(A.size() > B.size()) return true;
-      if(A.size() == B.size()){
-          for(int i=0;i<A.size();i++){
-	     int res = A.get(i).compareTo(B.get(i));
-	     if(res<0) return true;
-	     if(res>0) break;
-	  }
-       }
-       return false;
-    }
+	private static boolean doesAreplaceB(List<String> A, List<String> B){
+		if(A.size() > B.size()) return true;
+		if(A.size() == B.size()){
+			for(int i=0;i<A.size();i++){
+				int res = A.get(i).compareTo(B.get(i));
+				if(res<0) return true;
+				if(res>0) break;
+			}
+		}
+		return false;
+	}
 }
+
+/*
+Time complexity: Forming adjacency list: O(no of pairs) + DFS: every item as a start, and every item will traverse the whole graph in the worst case, so O(no of items OR no of pairs) * no of pairs * string comparisions
+Space complexity: HashMap for adjacency list: O(no of pairs) + visitedItem Set: O(no of items OR no of pairs) + temp result & result: O(no of pairs) + stack: O(no of items OR no of pairs) => O(no of pairs)
+*/
